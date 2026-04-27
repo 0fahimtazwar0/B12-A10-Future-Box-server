4a3b269018d2f14da777d8f628c10cd581de939f
0fahimtazwar0 <0fahimtazwar0@gmail.com>
2026-04-27 22:33:55 +0600

    implemented post add-book

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/add-book", async (req, res) => {
+      const newBook = req.body;
+      console.log("hitting post api", newBook);
+      const result = await booksColl.insertOne(newBook);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
